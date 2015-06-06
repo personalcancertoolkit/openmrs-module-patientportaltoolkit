2ebd28cda02387c077172d09c9c039e6f244b631
maurya <maurya.wolverine@gmail.com>
2015-06-06 02:16:23 -0400

    granulated encounter calls for treatments

--- omod/src/main/java/org/openmrs/module/patientportaltoolkit/web/controller/PatientPortalEncounterController.java
+++ omod/src/main/java/org/openmrs/module/patientportaltoolkit/web/controller/PatientPortalEncounterController.java
@@ -2,6 +2,7 @@ package org.openmrs.module.patientportaltoolkit.web.controller;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.Encounter;
 import org.openmrs.Patient;
 import org.openmrs.Person;
 import org.openmrs.api.context.Context;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,6 +22,9 @@ import java.util.List;
 @Controller
 public class PatientPortalEncounterController {
 
+    private final static String  RADIATION_ENCOUNTER = "CANCER TREATMENT - RADIATION";
+    private final static String  CHEMOTHERAPY_ENCOUNTER = "CANCER TREATMENT - CHEMOTHERAPY";
+    private final static String  SURGERY_ENCOUNTER = "CANCER TREATMENT - SURGERY";
     protected final Log log = LogFactory.getLog(getClass());
     @RequestMapping( value = "/patientportaltoolkit/getallencountertypes")
     @ResponseBody
@@ -34,5 +39,40 @@ public class PatientPortalEncounterController {
     {
         Patient patient=Context.getPatientService().getPatientByUuid(patientId);
         return ToolkitResourceUtil.generateEncounters(Context.getEncounterService().getEncountersByPatient(patient));
+    }
+
+    @RequestMapping( value = "/patientportaltoolkit/getchemotherapyencountersforpatient/{patientId}")
+    @ResponseBody
+    public Object getPatientPortalChemotherapyEncountersforPatient( @PathVariable( "patientId" ) String patientId)
+    {
+        Patient patient=Context.getPatientService().getPatientByUuid(patientId);
+        return ToolkitResourceUtil.generateEncounters(getEncountersByTreatment(patient, CHEMOTHERAPY_ENCOUNTER));
+    }
+
+    @RequestMapping( value = "/patientportaltoolkit/getradiationencountersforpatient/{patientId}")
+    @ResponseBody
+    public Object getPatientPortalRadiationEncountersforPatient( @PathVariable( "patientId" ) String patientId)
+    {
+        Patient patient=Context.getPatientService().getPatientByUuid(patientId);
+        return ToolkitResourceUtil.generateEncounters(getEncountersByTreatment(patient, RADIATION_ENCOUNTER));
+    }
+
+    @RequestMapping( value = "/patientportaltoolkit/getsurgeryencountersforpatient/{patientId}")
+    @ResponseBody
+    public Object getPatientPortalSurgeryEncountersforPatient( @PathVariable( "patientId" ) String patientId)
+    {
+        Patient patient=Context.getPatientService().getPatientByUuid(patientId);
+        return ToolkitResourceUtil.generateEncounters(getEncountersByTreatment(patient, SURGERY_ENCOUNTER));
+    }
+
+    public List<Encounter> getEncountersByTreatment(Patient patient,String treatmentType) {
+        List<Encounter> encounters = Context.getEncounterService().getEncountersByPatient(patient);
+        List<Encounter> treatmentEncounters = new ArrayList<Encounter>();
+        for (Encounter encounter : encounters) {
+            if (!encounter.isVoided() && treatmentType.equals(encounter.getEncounterType().getName())) {
+                treatmentEncounters.add(encounter);
+            }
+        }
+        return treatmentEncounters;
     }
 }
