6370f646930bd3ecdbb79b907662f641870f729f
maurya <maurya@openmrs.org>
2016-07-13 12:53:09 -0400

    #34 showing the users Login id

---
+++ omod/src/main/java/org/openmrs/module/patientportaltoolkit/fragment/controller/PatientPortalNavFragmentController.java
@@ -0,0 +1,24 @@
+package org.openmrs.module.patientportaltoolkit.fragment.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.patientportaltoolkit.api.util.PPTLogAppender;
+import org.openmrs.ui.framework.fragment.FragmentModel;
+import org.openmrs.ui.framework.page.PageRequest;
+
+/**
+ * Created by maurya on 7/13/16.
+ */
+public class PatientPortalNavFragmentController {
+
+    protected final Log log = LogFactory.getLog(getClass());
+
+    public void controller(FragmentModel model, PageRequest pageRequest) {
+
+        model.addAttribute("username", Context.getAuthenticatedUser().getUsername());
+        log.info(PPTLogAppender.appendLog("REQUEST_NAVIGATION_FRAGMENT", pageRequest.getRequest()));
+
+    }
+
+}
