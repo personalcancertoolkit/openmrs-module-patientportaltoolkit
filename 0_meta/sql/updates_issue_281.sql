e8fe7c53be33c7be7190d61fc3bf696d006f3c11
maurya <maurya@openmrs.org>
2017-08-24 11:23:32 -0400

    #281 database changes added

---
+++ 0_meta/sql/updates_issue_281.sql
@@ -0,0 +1,5 @@
+#################
+## 7.26.17 - UK
+#################
+## used to HIV Screening Result
+DELETE FROM `patientportal_form_conceptmapping` WHERE `patientportal_form_conceptmapping`.`concept_id` = 162955;
