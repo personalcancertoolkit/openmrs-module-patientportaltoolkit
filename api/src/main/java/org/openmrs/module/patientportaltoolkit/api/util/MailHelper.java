d841013473e8edfe0b50b32ab5909a6e43b4ed68
Sri Maurya Kummamuru <maurya@openmrs.org>
2022-05-12 14:06:22 -0400

    adding changes for smtp port and host can be configurable

--- api/src/main/java/org/openmrs/module/patientportaltoolkit/api/util/MailHelper.java
+++ api/src/main/java/org/openmrs/module/patientportaltoolkit/api/util/MailHelper.java
@@ -25,12 +25,12 @@ public class MailHelper {
     public static void sendMail (String subject, String content, String toEmail) {
 
         Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.host", Context.getAdministrationService().getGlobalProperty("patientportaltoolkit.smtpHost"));
+        props.put("mail.smtp.socketFactory.port", Context.getAdministrationService().getGlobalProperty("patientportaltoolkit.smtpPort"));
         props.put("mail.smtp.socketFactory.class",
                 "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.port", Context.getAdministrationService().getGlobalProperty("patientportaltoolkit.smtpPort"));
 
         Session session = Session.getInstance(props, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
